35fbe1f0c7644d217d9d9a4c4d3c3e55c52fb2fd
Joshua Wu <Joshua.wu.0910@gmail.com>
2021-07-26 23:48:38 -0700

    add esc and tab

--- keyboards/mechwild/murphpad/keymaps/josh-custom/keymap.c
+++ keyboards/mechwild/murphpad/keymaps/josh-custom/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 				  KC_F1,   KC_F2,   KC_F3,   KC_F4,
 				  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
 				  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-		TG(1),    KC_P4,   KC_P5,   KC_P6,   KC_TRNS,
+		TG(4),    KC_P4,   KC_P5,   KC_P6,   KC_TRNS,
 		KC_BSPC,  KC_P1,   KC_P2,   KC_P3,   MO(2),
 		KC_ENT,   KC_P0,   KC_PDOT, KC_LEFT, KC_RGHT,
         _______, _______, _______
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			     _______, _______, _______, _______,
 			     KC_1,    KC_2,    KC_3,    KC_4,
 			     KC_Q,    KC_W,    KC_E,    KC_R,
-		_______, KC_A,    KC_S,    KC_D,    KC_F,
+		KC_TRNS, KC_A,    KC_S,    KC_D,    KC_F,
 		KC_LFFT, KC_Z,    KC_X,    KC_C,    KC_V,
-		KC_LCTL, _______, _______, KC_LALT, KC_SPC,
+		KC_LCTL, KC_ESC,  KC_TAB, KC_LALT, KC_SPC,
 			     _______, _______, _______
 	)
 };
